ca09a78d687f9e7fe071b579e1daf2740db0e84b
Shubhadeep Das <shubhadeep.das2016@vitstudent.ac.in>
2019-03-13 00:19:47 +0530

    Login and register page redesign

--- taskmanager/accounts/views.py
+++ taskmanager/accounts/views.py
@@ -36,9 +36,8 @@ def login_view(request):
 
 
 def logout_view(request):
-    if request.method == 'POST':
-        logout(request)
-        return redirect('accounts:login')
+    logout(request)
+    return redirect('accounts:login')
 
 
 # TODO Delete after test
--- taskmanager/tasks/models.py
+++ taskmanager/tasks/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
 
 
 class Task(models.Model):
@@ -13,6 +14,12 @@ class Task(models.Model):
     description = models.CharField(max_length=300)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     assignee = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
+    created_date = models.DateField(
+        default=timezone.now, blank=True, null=True)
+    created_by = models.ForeignKey(
+        User, null=True, related_name='task_created_by', on_delete=models.CASCADE)
+    assigned_to = models.ForeignKey(
+        User, blank=True, null=True, related_name='task_assigned_to', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
