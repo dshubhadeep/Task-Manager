38f9e4d026e68cca4434ca79e9f2a4c6b055cd6f
Shubhadeep Das <shubhadeep.das2016@vitstudent.ac.in>
2019-03-27 16:06:02 +0530

    Added edit page

--- taskmanager/tasks/urls.py
+++ taskmanager/tasks/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from . import views
 
-app_name="tasks"
+app_name = "tasks"
 
 urlpatterns = [
-    path('create/', views.create_task, name='create')
+    path('create/', views.create_task, name='create'),
+    path('<int:task_id>/', views.detail, name='detail'),
+    path('edit/', views.edit, name='edit')
 ]
-
--- taskmanager/tasks/views.py
+++ taskmanager/tasks/views.py
@@ -25,3 +25,40 @@ def create_task(request):
     users = User.objects.all()
 
     return render(request, 'create.html', {'users': users})
+
+# TODO Viewing permission
+@login_required(login_url='accounts:login')
+def detail(request, task_id):
+
+    task = Task.objects.get(pk=task_id)
+    users = User.objects.all()
+
+    statuses = [('P', 'Planned'), ('O', 'Ongoing'), ('D', 'Done')]
+
+    return render(request, "detail.html", {
+        "id": task_id, "task": task,
+        "users": users, "statuses": statuses
+    })
+
+
+@login_required(login_url='accounts:login')
+def edit(request):
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        status = request.POST.get('status')
+        # TODO Add multiple assignees
+        assignee = request.POST.get('assignee')
+        assigned_to = User.objects.get(username=assignee)
+
+        task = Task.objects.get(pk=request.POST.get("task_id"))
+        task.title = title
+        task.description = description
+        task.assigned_to = assigned_to
+        task.status = status
+        task.save()
+
+        return redirect('accounts:home')
+
+    return redirect('accounts:home')
