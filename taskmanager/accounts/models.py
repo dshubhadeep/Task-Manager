11436a6677e89e02fc482ca41118c7a496a6ec55
Shubhadeep Das <shubhadeep.das2016@vitstudent.ac.in>
2019-03-29 19:13:55 +0530

    Added team model

--- taskmanager/accounts/models.py
+++ taskmanager/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=50)
+    created_by = models.ManyToManyField(User)
+    members = models.ForeignKey(
+        User, null=True, on_delete=models.CASCADE, related_name='team_created_by')
+
+    def __str__(self):
+        return self.team_name
+
+    class Meta:
+        ordering = ('team_name', )
